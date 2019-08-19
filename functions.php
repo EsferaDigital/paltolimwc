8a0f60b7deca0f20fe6636ca182ffa4117b0e585
Gabriel Zavando <gabrielzavando@gmail.com>
2019-08-19 14:31:04 -0400

    cambios en el footer y añadi una categoria y un producto

--- functions.php
+++ functions.php
@@ -149,6 +149,13 @@ if(!function_exists('paltolim_setup')):
 endif;
 add_action('after_setup_theme', 'paltolim_setup');
 
+//Permitimos archivos svg en multimedia
+function dmc_add_svg_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'dmc_add_svg_mime_types');
+
 // Soportes adicionales
 require_once get_template_directory() . '/inc/custom-header.php';
 require_once get_template_directory() . '/inc/custom-excerpt.php';
--- parts-template/footer-widgets.php
+++ parts-template/footer-widgets.php
@@ -5,8 +5,10 @@
   else:
   ?>
   <article class="Widget">
-    <h3><?php _e('Buscar', 'paltolim'); ?></h3>
-      <?php get_search_for(); ?>
+    <h3>¿Cómo comprar?</h3>
+    <a class="Como-comprar" href="http://localhost:8080/PaltolimWC/wp-content/uploads/2019/08/comprar.svg" target="_blank">
+      <img src="http://localhost:8080/PaltolimWC/wp-content/uploads/2019/08/comprar.svg" alt="Como comprar">
+    </a>
   </article>
   <?php endif; ?>
 </div>
